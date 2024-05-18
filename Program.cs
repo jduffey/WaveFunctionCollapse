b48965fd1825d287e4137e852947ac9e2c4775e3
Jed Duffey <jedduffey@gmail.com>
2024-05-18 14:51:57 -0400

    Extract method for attempt to generate model

--- Program.cs
+++ Program.cs
@@ -64,25 +64,7 @@ static class Program
                 for (int k = 0; k < maxAttempts; k++)
                 {
                     Console.Write($"  - Attempt {k + 1}/{maxAttempts} --> ");
-                    int seed = randomValueOverride ?? random.Next();
-                    bool success = model.Run(seed, xelem.Get("limit", -1));
-                    if (success)
-                    {
-                        var filename = $"{outputDirectoryName}/{name} {seed}";
-                        var pngFilename = $"{filename}.png";
-                        model.Save(pngFilename);
-                        Console.Write($"DONE; wrote {pngFilename}");
-                        if (model is SimpleTiledModel stmodel && xelem.Get("textOutput", false))
-                        {
-                            var textFilename = $"{filename}.txt";
-                            File.WriteAllText(textFilename, stmodel.TextOutput());
-                            Console.Write($" & {textFilename}");
-                        }
-                        Console.WriteLine();
-                        break;
-                    }
-
-                    Console.WriteLine("CONTRADICTION");
+                    if (AttemptToGenerateModel(randomValueOverride, random, model, xelem, outputDirectoryName, name)) break;
                 }
             }
         }
@@ -94,6 +76,30 @@ static class Program
         {
             CalculateAndSaveHashes(outputDirectoryName, randomValueOverride.Value, elapsedMilliseconds);
         }
+    }
+
+    private static bool AttemptToGenerateModel(int? randomValueOverride, Random random, Model model, XElement xelem, string outputDirectoryName, string name)
+    {
+        int seed = randomValueOverride ?? random.Next();
+        bool success = model.Run(seed, xelem.Get("limit", -1));
+        if (success)
+        {
+            var filename = $"{outputDirectoryName}/{name} {seed}";
+            var pngFilename = $"{filename}.png";
+            model.Save(pngFilename);
+            Console.Write($"DONE; wrote {pngFilename}");
+            if (model is SimpleTiledModel stmodel && xelem.Get("textOutput", false))
+            {
+                var textFilename = $"{filename}.txt";
+                File.WriteAllText(textFilename, stmodel.TextOutput());
+                Console.Write($" & {textFilename}");
+            }
+            Console.WriteLine();
+            return true;
+        }
+
+        Console.WriteLine("CONTRADICTION");
+        return false;
     }
 
     private static int? GetFlagValue(string[] args, string flagName)
