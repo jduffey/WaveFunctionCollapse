4ef7ad3d37955f7375409b9f6a9770ca4655bff2
Jed Duffey <jedduffey@gmail.com>
2024-05-18 15:26:37 -0400

    Pass seed value via CLI for deterministic results (#1)
    The intended purpose of this PR is to allow for the creation of golden master records for characterization testing in order to assist with future refactoring.
    
    Notable changes:
    - Can pass a flag via the CLI to override the random seed integer used to generate the model
       - e.g. `dotnet run Program.cs --randomValueOverride 1559`
    - Creates a .txt file containing the name of each file generated in the output folder along with its SHA256 digest
    - Improve console logging messages

--- Program.cs
+++ Program.cs
@@ -3,23 +3,25 @@
 using System;
 using System.Xml.Linq;
 using System.Diagnostics;
+using System.IO;
+using WaveFunctionCollapse;
 
 static class Program
 {
-    static void Main()
+    private static void Main(string[] args)
     {
         Stopwatch sw = Stopwatch.StartNew();
-        var folder = System.IO.Directory.CreateDirectory("output");
-        foreach (var file in folder.GetFiles()) file.Delete();
 
-        Random random = new();
+        const string outputDirectoryName = "output";
+        Utils.PrepareOutputDirectory(outputDirectoryName);
+
         XDocument xdoc = XDocument.Load("samples.xml");
-
+        var randomValueOverride = Utils.GetFlagValue(args, "--randomValueOverride");
+        Random random = new Random();
         foreach (XElement xelem in xdoc.Root.Elements("overlapping", "simpletiled"))
         {
-            Model model;
             string name = xelem.Get<string>("name");
-            Console.WriteLine($"< {name}");
+            Console.WriteLine($"Sample name: {name}");
 
             bool isOverlapping = xelem.Name == "overlapping";
             int size = xelem.Get("size", isOverlapping ? 48 : 24);
@@ -27,8 +29,14 @@ static class Program
             int height = xelem.Get("height", size);
             bool periodic = xelem.Get("periodic", false);
             string heuristicString = xelem.Get<string>("heuristic");
-            var heuristic = heuristicString == "Scanline" ? Model.Heuristic.Scanline : (heuristicString == "MRV" ? Model.Heuristic.MRV : Model.Heuristic.Entropy);
+            var heuristic = heuristicString switch
+            {
+                "Scanline" => Model.Heuristic.Scanline,
+                "MRV" => Model.Heuristic.MRV,
+                _ => Model.Heuristic.Entropy
+            };
 
+            Model model;
             if (isOverlapping)
             {
                 int N = xelem.Get("N", 3);
@@ -46,26 +54,49 @@ static class Program
                 model = new SimpleTiledModel(name, subset, width, height, periodic, blackBackground, heuristic);
             }
 
-            for (int i = 0; i < xelem.Get("screenshots", 2); i++)
+            var maxScreenshots = randomValueOverride.HasValue ? 1 : xelem.Get("screenshots", 2);
+            for (int i = 0; i < maxScreenshots; i++)
             {
-                for (int k = 0; k < 10; k++)
+                Console.WriteLine($" - Screenshot {i + 1}/{maxScreenshots}");
+                var maxAttempts = randomValueOverride.HasValue ? 1 : 10;
+                for (int k = 0; k < maxAttempts; k++)
                 {
-                    Console.Write("> ");
-                    int seed = random.Next();
-                    bool success = model.Run(seed, xelem.Get("limit", -1));
-                    if (success)
-                    {
-                        Console.WriteLine("DONE");
-                        model.Save($"output/{name} {seed}.png");
-                        if (model is SimpleTiledModel stmodel && xelem.Get("textOutput", false))
-                            System.IO.File.WriteAllText($"output/{name} {seed}.txt", stmodel.TextOutput());
-                        break;
-                    }
-                    else Console.WriteLine("CONTRADICTION");
+                    Console.Write($"  - Attempt {k + 1}/{maxAttempts} --> ");
+                    if (AttemptToGenerateModel(randomValueOverride, random, model, xelem, outputDirectoryName, name)) break;
                 }
             }
         }
 
-        Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+        var elapsedMilliseconds = sw.ElapsedMilliseconds;
+        Console.WriteLine($"time = {elapsedMilliseconds} ms for generating output");
+
+        if (randomValueOverride is not null)
+        {
+            Utils.CalculateAndSaveHashes(outputDirectoryName, randomValueOverride.Value, elapsedMilliseconds);
+        }
+    }
+
+    private static bool AttemptToGenerateModel(int? randomValueOverride, Random random, Model model, XElement xelem, string outputDirectoryName, string name)
+    {
+        int seed = randomValueOverride ?? random.Next();
+        bool success = model.Run(seed, xelem.Get("limit", -1));
+        if (success)
+        {
+            var filename = $"{outputDirectoryName}/{name} {seed}";
+            var pngFilename = $"{filename}.png";
+            model.Save(pngFilename);
+            Console.Write($"DONE; wrote {pngFilename}");
+            if (model is SimpleTiledModel stmodel && xelem.Get("textOutput", false))
+            {
+                var textFilename = $"{filename}.txt";
+                File.WriteAllText(textFilename, stmodel.TextOutput());
+                Console.Write($" & {textFilename}");
+            }
+            Console.WriteLine();
+            return true;
+        }
+
+        Console.WriteLine("CONTRADICTION");
+        return false;
     }
 }
